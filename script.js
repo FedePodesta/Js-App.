db51bcdd9e617f9e30e04c46715b5efe97faa593
FedePodesta <federicoandrespodesta@gmail.com>
2022-09-22 14:59:22 -0300

    Refactorización y arreglos

--- script.js
+++ script.js
@@ -3,20 +3,112 @@ const modal = document.getElementById('modal');
 const close = document.getElementById('close');
 const background = document.getElementById('background');
 const modalContenido = document.getElementById('modal-contenido');
+const loader = document.getElementById('loader');
 
 const listadoUsuarios = document.getElementById('listado-usuarios');
 
-close.addEventListener('click', e => {
-    modal.classList.remove('show');
-});
+const estado = {
+    usuarios: null
+};
 
-background.addEventListener('click', e => {
+function showLoader(show) {
+    if (show) {
+        loader.classList.add('show');
+    } else {
+        loader.classList.remove('show');
+    }
+}
+
+function realizarPeticion(metodo, url, handleOk, handleError, datos) {
+    const xhr = new XMLHttpRequest();
+
+    showLoader(true);
+
+    xhr.open(metodo, url);
+
+    xhr.responseType = 'json';
+
+    xhr.addEventListener('load', e => {
+        if (xhr.status >= 200 && xhr.status <= 299) {
+            console.log(xhr);
+            handleOk(xhr.response);
+        } else {
+            handleError(xhr.status);
+        }
+        showLoader(false);
+    });
+
+    xhr.addEventListener('error', e => {
+        handleError('Error');
+        showLoader(false);
+    });
+
+    xhr.send(JSON.stringify(datos));
+}
+
+function handleError(error) {
+    alert(error);
+}
+
+function mostrarUsuarios(usuarios) {
+    estado.usuarios = usuarios;
+    usuarios.forEach(mostrarUsuario)
+}
+
+function eliminarUsuario() {
+    const article = document.getElementById(`usuario-${estado.usuarioEliminado}`);
+
+    if (article) {
+        article.remove();
+    }
+}
+
+function ocultarModal() {
     modal.classList.remove('show');
-});
+}
+
+function handleEliminar(event) {
+    const id = parseInt(event.target.parentNode.id.split('-')[1]);
+    const usuario = estado.usuarios.find(u => u.id === id);
+
+    modalContenido.innerHTML = `
+        <h3>¿Esta seguro que desea eliminar el usuario ${usuario.name}?</h3>
+    `;
+
+    const btnCancelar = document.createElement('button');
+    btnCancelar.className = 'btn btn-cancel';
+    btnCancelar.textContent = 'Cancelar';
+
+    btnCancelar.addEventListener('click', e => {
+        modal.classList.remove('show');
+    });
+
+    const btnOk = document.createElement('button');
+    btnOk.className = 'btn btn-ok';
+    btnOk.textContent = 'OK';
+
+    btnOk.addEventListener('click', e => {
+        estado.usuarioEliminado = usuario.id;
+
+        realizarPeticion(
+            'delete',
+            `https://jsonplaceholder.typicode.com/users/${usuario.id}`,
+            eliminarUsuario,
+            handleError
+        )
+        modal.classList.remove('show');
+    });
+
+    modalContenido.append(btnCancelar, btnOk)
+
+    modal.classList.add('show');
+}
+
 
 function mostrarUsuario(usuario) {
     const article = document.createElement('article');
     article.className = 'usuario';
+    article.id = `usuario-${usuario.id}`;
 
     article.innerHTML = `
         <h2>${usuario.name}</h2>
@@ -75,26 +167,18 @@ function mostrarUsuario(usuario) {
         btnGuardar.textContent = 'Guardar';
 
         btnGuardar.addEventListener('click', e => {
-            const x = new XMLHttpRequest();
-
-            x.open('put', `https://jsonplaceholder.typicode.com/users/${usuario.id}`);
-
-            x.addEventListener('load', e => {
-                if (x.status >= 200 && x.status <= 299) {
-                    alert('Modificado!')
-                } else {
-                    alert('Error');
+            realizarPeticion(
+                'put',
+                `https://jsonplaceholder.typicode.com/users/${usuario.id}`,
+                () => {},
+                handleError,
+                {
+                    name: nombre.value,
+                    email: email.value,
+                    phone: telefono.value,
+                    website: web.value
                 }
-            });
-
-            const data = JSON.stringify({
-                name: nombre.value,
-                email: email.value,
-                phone: telefono.value,
-                website: web.value
-            });
-
-            x.send(data);
+            )
 
             modal.classList.remove('show');
         });
@@ -108,67 +192,14 @@ function mostrarUsuario(usuario) {
     btnEliminar.className = 'btn btn-cancel';
     btnEliminar.textContent = 'Eliminar';
 
-    btnEliminar.addEventListener('click', e => {
-        modalContenido.innerHTML = `
-            <h3>¿Esta seguro que desea eliminar el usuario ${usuario.name}?</h3>
-        `;
-
-        const btnCancelar = document.createElement('button');
-        btnCancelar.className = 'btn btn-cancel';
-        btnCancelar.textContent = 'Cancelar';
-
-        btnCancelar.addEventListener('click', e => {
-            modal.classList.remove('show');
-        });
-
-        const btnOk = document.createElement('button');
-        btnOk.className = 'btn btn-ok';
-        btnOk.textContent = 'OK';
-
-        btnOk.addEventListener('click', e => {
-            const x = new XMLHttpRequest();
-
-            x.open('delete', `https://jsonplaceholder.typicode.com/users/${usuario.id}`);
-
-            x.addEventListener('load', e => {
-                if (x.status >= 200 && x.status <= 299) {
-                    article.remove();
-                } else {
-                    alert('Error');
-                }
-            });
-
-            x.send();
-
-            modal.classList.remove('show');
-        });
-
-        modalContenido.append(btnCancelar, btnOk)
-
-        modal.classList.add('show');
-    });
+    btnEliminar.addEventListener('click', handleEliminar);
 
     article.append(btnModificar, btnEliminar);
 
     listadoUsuarios.append(article);            
 }
 
-const xhr = new XMLHttpRequest();
+close.addEventListener('click', ocultarModal);
+background.addEventListener('click', ocultarModal);
 
-xhr.open('get', 'https://jsonplaceholder.typicode.com/users');
-
-xhr.responseType = 'json';
-
-xhr.addEventListener('load', e => {
-    if (xhr.status === 200) {
-        xhr.response.forEach(mostrarUsuario);
-    } else {
-        console.error('Load (Error)')
-    }
-});
-
-xhr.addEventListener('error', e => {
-    console.error('Error');
-});
-
-xhr.send();
+realizarPeticion('get', 'https://jsonplaceholder.typicode.com/users', mostrarUsuarios,handleError);
